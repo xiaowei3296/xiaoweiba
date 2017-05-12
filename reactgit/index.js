61a3bedc977c5d67ea8f688b9972ec7d5d70db87
angel liu <xiaowei3296@163.com>
2017-05-12 18:41:38 +0800

    test
    test

---
+++ reactgit/index.js
@@ -0,0 +1,3 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(<h1>HelloWorld</h1>,document.getElementById("wrap"));
--- reactgit/src/helloword.js
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-    <h1>aaf</h1>,
-    Document.getElementById('example')
-)
